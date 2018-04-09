815cac4edc4dcf0f6a2660c0022b455f6007c6f4
liushaozong1206 <641753538@qq.com>
2018-04-09 14:22:16 +0800

    图片

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: '192.168.84.43',
-    port: '8060',
+    host: '192.168.84.1',
+    port: '8030',
     publicPath: '../',
     vendors: [
         'axios',
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ var express = require('express')
 var router = express.Router()
 
 /* GET home page. */
-router.get('/aaa', function (req, res, next) {
-    data
-    res.render('index', {data: data})
+router.get('/', function (req, res, next) {
+    res.render('index')
 })
 
 module.exports = router
