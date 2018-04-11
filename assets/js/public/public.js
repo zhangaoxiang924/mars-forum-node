0f92c5e341508e66941ea3e1790d2404ec6cfa7e
liushaozong1206 <641753538@qq.com>
2018-04-11 17:19:55 +0800

    冲突

--- assets/js/public/public.js
+++ assets/js/public/public.js
@@ -5,6 +5,7 @@
  */
 
 import axios from 'axios'
+import Cookies from 'js-cookie'
 
 const isPc = () => {
     const userAgent = window.navigator.userAgent.toLowerCase()
@@ -101,8 +102,18 @@ const pageLoadingHide = () => {
         }
     })
  */
+
+// 验证手机号
+const isPoneAvailable = (pone) => {
+    const myreg = /^[1][3,4,5,7,8][0-9]{9}$/
+    if (!myreg.test(pone)) {
+        return false
+    } else {
+        return true
+    }
+}
 const axiosAjax = (arg) => {
-    const {type, url, params, contentType, formData, fn} = arg
+    const {type, url, params, fn, contentType, formData} = arg
 
     const ajaxLoadingStr = `<div class="lk-loading ajax active" id="ajaxLoading">
     <div class="lk-loading-center">
@@ -124,13 +135,19 @@ const axiosAjax = (arg) => {
         opt = {
             method: type,
             url: url,
-            data: params
+            params: params
         }
     } else if (ajaxType === 'get') {
         opt = {
             method: type,
             url: url,
             params: params
+        }
+    } else if (ajaxType === 'complexpost') {
+        opt = {
+            method: 'post',
+            url: url,
+            data: params
         }
     }
 
@@ -165,6 +182,181 @@ const axiosAjax = (arg) => {
     })
 }
 
+// 登陆/注册时设置 cookies
+const setCookies = (obj) => {
+    for (let key in obj) {
+        Cookies.set('hx_forum_' + key, obj[key], {expires: 7})
+    }
+}
+
+// 注销时删除相关cookies
+const deleteCookies = () => {
+    let strcookie = document.cookie
+    let arrcookie = strcookie.split('; ')
+    for (let i = 0; i < arrcookie.length; i++) {
+        let arr = arrcookie[i].split('=')
+        if (arr[0].indexOf('hx_forum') !== -1) {
+            Cookies.remove(arr[0])
+        }
+    }
+}
+
+const utils = {
+    banner: function () {
+        // 根据是否登陆展示不同按钮
+        if (Cookies.get('hx_forum_token') === undefined) {
+            $('.login-registration').html(`<p class="login noColorBtn">登录</p><p class="registration colorBtn">注册</p>`)
+        } else {
+            $('.login-registration').html(`<p class="userName noColorBtn" title=${Cookies.get('hx_forum_nickName')}>${Cookies.get('hx_forum_nickName')}</p><p class="logOut colorBtn">注销</p>`)
+        }
+        // 弹出登陆框
+        $('.login').click(function (e) {
+            e.stopPropagation()
+            $('.shade').show()
+            $('.login-con, .login-con .login').show()
+        })
+
+        // 弹出注册框
+        $('.registration').click(function (e) {
+            e.stopPropagation()
+            $('.shade').show()
+            $('.login-con, .login-con .register').show()
+        })
+
+        // 隐藏注册/登陆框
+        $('.shade').click(function () {
+            if ($('.login-con').is(':visible')) {
+                $('.shade').hide()
+                $('.login-con, .login-con .login, .login-con .register').hide()
+            }
+        })
+
+        // 登陆请求
+        $('.login-btn').click(function () {
+            let $phoneInput = $('.phone-input').val()
+            let $password = $('.password-input').val()
+            if ($phoneInput.trim() === '' || !isPoneAvailable($phoneInput)) {
+                layer.msg('手机号码有误，请核对后重新输入！')
+                return false
+            }
+            if ($password.trim() === '') {
+                layer.msg('密码不能为空！')
+                return false
+            }
+            axiosAjax({
+                type: 'post',
+                url: '/api/pc/passport/account/login',
+                params: {
+                    phonenum: $phoneInput,
+                    password: $password
+                },
+                fn: (data) => {
+                    if (data.code !== 1) {
+                        layer.msg(data.msg)
+                    } else {
+                        setCookies(data.obj)
+                        $('.shade').hide()
+                        $('.login-con, .login-con .login').hide()
+                        window.location.reload()
+                        layer.msg('登陆成功！')
+                    }
+                }
+            })
+        })
+        // 注销
+        $('.login-registration').on('click', '.logOut', function () {
+            deleteCookies()
+            layer.msg('已注销！')
+            window.location.reload()
+        })
+        // 注册获取验证码
+        $('.getCode').click(function () {
+            axiosAjax({
+                type: 'post',
+                url: '/api/pc/passport/account/getverifcode',
+                params: {
+                    countrycode: 86,
+                    verifcategory: 1, // 验证码类别 1 注册 2 找回密码
+                    phonenum: $('.register-phone-num input').val()
+                },
+                fn: (data) => {
+                    if (data.code !== 1) {
+                        layer.msg(data.msg)
+                    } else {
+                        layer.msg('验证码已发送，请注意查收！')
+                    }
+                }
+            })
+        })
+        // 注册请求
+        $('.register-btn').click(function () {
+            let $authCode = $('.auth-code-item input').val()
+            let $password = $('.register-pw input').val()
+            let $passwordConfirm = $('.register-pw-confirm input').val()
+            let $phoneInput = $('.register-phone-num input').val()
+            let reg = /^\d{6}$/ // 6位数字正则
+
+            if ($phoneInput.trim() === '' || !isPoneAvailable($phoneInput)) {
+                layer.msg('手机号码有误，请核对后重新输入！')
+                return false
+            }
+
+            if ($authCode.trim() === '') {
+                layer.msg('验证码不能为空！')
+                return false
+            }
+            if (!reg.test($authCode)) {
+                layer.msg('验证码格式有误，请重新输入！')
+                return false
+            }
+
+            if ($password.trim() === '') {
+                layer.msg('密码不能为空！')
+                return false
+            }
+
+            if ($password !== $passwordConfirm) {
+                layer.msg('密码输入不一致，请重新输入！')
+                return false
+            }
+
+            axiosAjax({
+                type: 'post',
+                url: '/api/pc/passport/account/register',
+                params: {
+                    verifcode: $authCode,
+                    password: $password,
+                    verifcategory: 1, // 验证码类别 1 注册 2 找回密码
+                    phonenum: $phoneInput
+                },
+                fn: (data) => {
+                    if (data.code !== 1) {
+                        layer.msg(data.msg)
+                    } else {
+                        setCookies(data.obj)
+                        $('.shade').hide()
+                        $('.login-con, .login-con .register').hide()
+                        window.location.reload()
+                        layer.msg('注册成功！')
+                    }
+                }
+            })
+        })
+
+        $('.to-login').click(function (e) {
+            e.stopPropagation()
+            $('.login-con .register').hide()
+            $('.login-con .login').show()
+        })
+
+        $('.to-register').click(function (e) {
+            e.stopPropagation()
+            $('.login-con .register').show()
+            $('.login-con .login').hide()
+        })
+    }
+}
+
 const proxyUrlBbs = '/api/bbs'
 const proxyUrlPc = '/api/pc'
 
@@ -178,6 +370,9 @@ export {
     getQueryString,
     pageLoadingHide,
     axiosAjax,
+    deleteCookies,
+    isPoneAvailable,
+    utils,
     proxyUrlBbs,
     proxyUrlPc
 }
